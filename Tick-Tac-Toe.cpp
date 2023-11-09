85acf50bbe2e0ee3d7d11ad6265e97783eeab239
VishnuA97 <97956384+VishnuA97@users.noreply.github.com>
2023-11-09 19:06:18 +0530

    Add files via upload

---
+++ Tick-Tac-Toe.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void grid() {
+    cout<<"         |       |       "<<endl;
+    cout<<"         |       |       "<<endl;
+    cout<<"         |       |       "<<endl;
+    cout<<" -------------------------"<<endl;
+    cout<<"         |       |       "<<endl;
+    cout<<"         |       |       "<<endl;
+    cout<<"         |       |       "<<endl;
+    cout<<" -------------------------"<<endl;
+    cout<<"         |       |       "<<endl;
+    cout<<"         |       |       "<<endl;
+    cout<<"         |       |       "<<endl;
+
+}
+
+int main(){
+    grid();
+
+    return 0;
+}
+
