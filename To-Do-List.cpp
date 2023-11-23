5183701597d01bda2fa1cf747f9e045b3990c991
VishnuA97 <97956384+VishnuA97@users.noreply.github.com>
2023-11-23 16:30:08 +0530

    Add files via upload

---
+++ To-Do-List.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+
+fstream file;
+string task;
+char menu, ch;
+
+void main_menu(){
+    cout<<"\t\t\t\t\t\tTo-Do List\n\n\n";
+    cout<<"1.  Add a new task\n";
+    cout<<"2.  View the list\n";
+    cout<<"3.  Mark a task as completed\n";
+    cout<<"4.  Delete a task\n\n\n";
+}
+
+void add_task(){
+    system("cls");
+    cout << "Enter Task:";
+    getline(cin,task);
+    cin.get();
+    
+    file.open("task.txt", ios::app );
+    if(file.is_open()){
+        file << task<<"\n";
+        file.close();
+    }  
+    cout<<"Do you want to add another task?(y/n): \n";
+    cin >> ch;
+    if(ch == 'y'){
+        add_task();
+    }
+    else{
+        exit(0);
+    }
+
+}
+
+void view_task(){}
+
+void complete_task(){}
+
+void delete_task(){}
+
+
+
+void to_do_list(){
+    int choice;
+   
+    while(true){
+        main_menu();
+        cout<<"Enter a choice to continue\n";
+        cin>>choice;
+        switch(choice){
+            case 1: add_task(); break;
+            case 2: view_task(); break;
+            case 3: complete_task(); break;
+            case 4: delete_task(); break;
+            default: cout<<"Invalid choice!!! Enter a number between 1 and 4\n"; 
+            break;
+        }
+    }
+}    
+
+int main(){
+    while(true){
+        add_task();
+    }
+    return 0;
+}
